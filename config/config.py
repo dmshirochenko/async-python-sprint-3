7da7e5acebeaaf26701d8eeb8519172113ae8565
Dmitriy Shirochenko <d.shirochenko@criteo.com>
2024-01-20 20:09:49 +0100

    code refactoring rest

--- config/config.py
+++ config/config.py
@@ -19,12 +19,15 @@ class HTTPError(BaseModel):
 class ErrorMessages(BaseModel):
     invalid_json_format: HTTPError = HTTPError(message="Invalid JSON format", status_code=400)
     missing_required_data: HTTPError = HTTPError(message="Missing required data", status_code=400)
+    user_has_not_been_found: HTTPError = HTTPError(message="User has not been found", status_code=400)
     unauthorized: HTTPError = HTTPError(message="Unauthorized", status_code=401)
     forbidden: HTTPError = HTTPError(message="Forbidden", status_code=403)
     not_found: HTTPError = HTTPError(message="Not Found", status_code=404)
     method_not_allowed: HTTPError = HTTPError(message="Method Not Allowed", status_code=405)
     internal_server_error: HTTPError = HTTPError(message="Internal Server Error", status_code=500)
     request_timeout_error: HTTPError = HTTPError(message="Request processing timed out", status_code=408)
+    message_limit_reached: HTTPError = HTTPError(message="Message limit reached", status_code=429)
+    database_error: HTTPError = HTTPError(message="Database error occurred", status_code=500)
 
 
 class Settings(BaseSettings):
@@ -32,6 +35,7 @@ class Settings(BaseSettings):
     app_debug_level: str = Field("INFO", env="APP_DEBUG_LEVEL")
     base_dir: str = Field(BASE_DIR)
     max_request_time: int = Field(5, env="MAX_REQUEST_TIME")
+    max_messages_per_hour: int = Field(20, env="MAX_MESSAGE_PER_HOUR")
     error_messages: ErrorMessages = ErrorMessages()
 
 
--- src/db_connector/postgres_connector.py
+++ src/db_connector/postgres_connector.py
@@ -1,47 +1,80 @@
 import asyncpg
+import logging.config
+from typing import Any, Optional
 
+from config.logger import LOGGING
 from src.backoff.backoff import retry_database_connection
+
+logging.config.dictConfig(LOGGING)
+logger = logging.getLogger(__name__)
 
 
 class AsyncDatabaseConnector:
-    def __init__(self, database_url):
-        self.database_url = database_url
-        self.connection = None
+    def __init__(self, database_url: str) -> None:
+        self.database_url: str = database_url
+        self.connection: Optional[asyncpg.Connection] = None
 
-    async def _establish_connection(self):
-        if self.connection is None or self.connection.is_closed():
-            self.connection = await asyncpg.connect(self.database_url)
+    async def _establish_connection(self) -> None:
+        try:
+            if self.connection is None or self.connection.is_closed():
+                self.connection = await asyncpg.connect(self.database_url)
+        except asyncpg.PostgresError as e:
+            logger.error("Error establishing database connection: %s", e)
+            raise e
 
     @retry_database_connection()
-    async def connect(self):
-        self.connection = await asyncpg.connect(self.database_url)
+    async def connect(self) -> None:
+        try:
+            self.connection = await asyncpg.connect(self.database_url)
+        except asyncpg.PostgresError as e:
+            logger.error("Error connecting to database: %s", e)
+            raise e
 
-    async def close(self):
+    async def close(self) -> None:
         if self.connection:
             await self.connection.close()
 
-    async def execute(self, query, *args, **kwargs):
-        if not self.connection:
-            await self.connect()
-        if kwargs:
-            return await self.connection.execute(query, *args, **kwargs)
-        else:
-            return await self.connection.execute(query, *args)
+    async def execute(self, query: str, *args: Any, **kwargs: Any) -> str:
+        try:
+            if not self.connection:
+                await self.connect()
+            if kwargs:
+                return await self.connection.execute(query, *args, **kwargs)
+            else:
+                return await self.connection.execute(query, *args)
+        except asyncpg.PostgresError as e:
+            logger.error("Error executing query: %s", e)
+            raise e
 
-    async def fetch(self, query, *args):
-        if not self.connection:
-            await self.connect()
-        return await self.connection.fetch(query, *args)
+    async def fetch(self, query: str, *args: Any) -> list:
+        try:
+            if not self.connection:
+                await self.connect()
+            return await self.connection.fetch(query, *args)
+        except asyncpg.PostgresError as e:
+            logger.error("Error fetching data: %s", e)
+            raise e
 
-    async def fetchrow(self, query, *args):
-        if not self.connection:
-            await self.connect()
-        return await self.connection.fetchrow(query, *args)
+    async def fetchrow(self, query: str, *args: Any) -> asyncpg.Record:
+        try:
+            if not self.connection:
+                await self.connect()
+            return await self.connection.fetchrow(query, *args)
+        except asyncpg.PostgresError as e:
+            logger.error("Error fetching row: %s", e)
+            raise e
+        except Exception as e:
+            logger.error("Error fetching row: %s", e)
+            raise e
 
-    async def fetchval(self, query, *args):
-        if not self.connection:
-            await self.connect()
-        return await self.connection.fetchval(query, *args)
+    async def fetchval(self, query: str, *args: Any) -> Any:
+        try:
+            if not self.connection:
+                await self.connect()
+            return await self.connection.fetchval(query, *args)
+        except asyncpg.PostgresError as e:
+            logger.error("Error fetching value: %s", e)
+            raise e
 
-    async def insert_data(self, query, *args, **kwargs):
+    async def insert_data(self, query: str, *args: Any, **kwargs: Any) -> str:
         return await self.execute(query, *args, **kwargs)
